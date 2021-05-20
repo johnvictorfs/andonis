d497c1355093b6cc04c54f6b9fef18b695fb76ef
John <johnvfs@protonmail.com>
2021-05-19 22:33:59 -0300

    Add image and title previews pop-up when searching for anime

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { createMuiTheme, ThemeProvider, CssBaseline } from '@material-ui/core'
 import { ApolloProvider } from '@apollo/client'
 
 import { anilistClient } from '@/services/anilist'
 import Routes from '@/Routes'
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  }
+})
+
 const App: React.FC = () => (
-  <ApolloProvider client={anilistClient}>
-    <Routes />
-  </ApolloProvider>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+
+    <ApolloProvider client={anilistClient}>
+      <Routes />
+    </ApolloProvider>
+  </ThemeProvider>
 )
 
 export default App
--- src/components/AnimeSearch.tsx
+++ src/components/AnimeSearch.tsx
@@ -3,12 +3,23 @@ import { useState } from 'react'
 import { gql, useLazyQuery } from '@apollo/client'
 
 import { fade, makeStyles } from '@material-ui/core/styles'
-import { CircularProgress, InputBase } from '@material-ui/core'
+import {
+  CircularProgress,
+  InputBase,
+  Popper,
+  Fade,
+  Paper,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon
+} from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
 
 const useStyles = makeStyles((theme) => ({
   circularProgress: {
-    marginLeft: theme.spacing(1),
+    marginLeft: theme.spacing(1)
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
@@ -17,24 +28,24 @@ const useStyles = makeStyles((theme) => ({
     pointerEvents: 'none',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   search: {
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
     '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+      backgroundColor: fade(theme.palette.common.white, 0.25)
     },
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
+      width: 'auto'
+    }
   },
   inputRoot: {
-    color: 'inherit',
+    color: 'inherit'
   },
   inputInput: {
     fontSize: 20,
@@ -45,10 +56,14 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up('sm')]: {
       width: '15ch',
       '&:focus': {
-        width: '25ch',
-      },
-    },
+        width: '25ch'
+      }
+    }
   },
+  searchResults: {
+    padding: theme.spacing(1),
+    backgroundColor: theme.palette.background.paper
+  }
 }))
 
 const SEARCH_ANIME = gql`
@@ -68,11 +83,23 @@ const SEARCH_ANIME = gql`
   }
 `
 
+interface Media {
+  id: number
+  coverImage: {
+    large: string
+    color: string
+  }
+  title: {
+    userPreferred: string
+  }
+}
+
 const AnimeSearch: React.FC = () => {
   const [query, setQuery] = useState('')
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const classes = useStyles()
 
-  const [getAnime, { loading }] = useLazyQuery(SEARCH_ANIME)
+  const [getAnime, { loading, data }] = useLazyQuery(SEARCH_ANIME)
 
   const searchAnime = () => {
     getAnime({ variables: { query } })
@@ -80,6 +107,7 @@ const AnimeSearch: React.FC = () => {
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value)
+    setAnchorEl(anchorEl ? null : event.currentTarget)
   }
 
   return (
@@ -96,7 +124,7 @@ const AnimeSearch: React.FC = () => {
           placeholder="Search Anime..."
           classes={{
             root: classes.inputRoot,
-            input: classes.inputInput,
+            input: classes.inputInput
           }}
           startAdornment={
             loading && (
@@ -115,6 +143,41 @@ const AnimeSearch: React.FC = () => {
           }}
         />
       </div>
+
+      <Popper
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        placement="bottom"
+        transition
+      >
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            {(data && (
+              <Paper>
+                <Grid container spacing={1}>
+                  <List component="nav" aria-label="anime-search-results">
+                    {data.Page.media.map((media: Media) => (
+                      <ListItem key={media.id} button>
+                        <ListItemIcon>
+                          <img
+                            src={media.coverImage.large}
+                            alt={media.title.userPreferred}
+                            width="48"
+                            height="64"
+                            style={{ marginRight: 5 }}
+                          />
+                        </ListItemIcon>
+
+                        <ListItemText primary={media.title.userPreferred} />
+                      </ListItem>
+                    ))}
+                  </List>
+                </Grid>
+              </Paper>
+            )) || <span />}
+          </Fade>
+        )}
+      </Popper>
     </>
   )
 }
